ed78d49319ebc814589a0942ee24499db382fa63
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2021-11-30 19:04:31 +0000

    Added navigation bar to TopicsScreen

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,3 +1,4 @@
+import 'package:fireship_app/shared/bottom_nav.dart';
 import 'package:flutter/material.dart';
 
 class TopicsScreen extends StatelessWidget {
@@ -5,6 +6,8 @@ class TopicsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 }
