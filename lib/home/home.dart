eb38dc8b7a300cb290e0cff7c963865de151b775
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2021-12-01 09:52:12 +0000

    Implemented Auth Stream

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:fireship_app/login/login.dart';
+import 'package:fireship_app/topics/topics.dart';
+import 'package:fireship_app/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: ElevatedButton(
-        child: Text(
-          'about',
-          style: Theme.of(context).textTheme.button,
-        ),
-        onPressed: () => Navigator.pushNamed(context, '/about'),
-      ),
-    ));
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading'); // to add method here
+        } else if (snapshot.hasError) {
+          return const Center (
+            child: Text('Error'), // to add method here
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      }
+    );
   }
 }
