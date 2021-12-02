ed0710bb881628dd4457867c6a94da421cddb254
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2021-12-02 22:26:22 +0000

    Relational data fetching methods

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -32,4 +32,16 @@ class FirestoreService {
     
     return Quiz.fromJson(snapshot.data() ?? {});
   }
+
+  // Listens to current user's report document in Firestore
+  Stream<Report> streamReport() {
+    return AuthService().userStream.switchMap((user) {
+      if (user != null) {
+        var ref = _db.collection('reports').doc(user.uid);
+        return ref.snapshots().map((doc) => Report.fromJson(doc.data()!));
+      } else {
+        return Stream.fromIterable([Report()]);
+      }
+    });
+  }
 }
